936f640b522d868cef3e7f338a015300f30725ad
Alberto Osorio <55248968+Betoso99@users.noreply.github.com>
2022-01-17 09:50:21 -0400

    Update trip_controller.dart

--- lib/src/controllers/trip_controller.dart
+++ lib/src/controllers/trip_controller.dart
@@ -42,30 +42,3 @@ Future<http.Response> getTouristSpots() async {
   );
   return response;
 }
-
-/*
-Future<http.Response> getTouristRoute(LatLng origin, LatLng destination) async {
-  final response = await http.get(Uri.parse(url+'destination=$destination&origin=$origin&mode=transit&key=AIzaSyB_iMzzl__vncphopqHr8r51Frw5H_q2eU'),
-    headers: {
-      HttpHeaders.contentTypeHeader: "application/json",
-    },
-  );
-  return response;
-}
-*/
-
-/*{
-  origin: LatLng | String | google.maps.Place,
-  destination: LatLng | String | google.maps.Place,
-  travelMode: TravelMode,
-  transitOptions: TransitOptions,
-  drivingOptions: DrivingOptions,
-  unitSystem: UnitSystem,
-  waypoints[]: DirectionsWaypoint,
-  optimizeWaypoints: Boolean,
-  provideRouteAlternatives: Boolean,
-  avoidFerries: Boolean,
-  avoidHighways: Boolean,
-  avoidTolls: Boolean,
-  region: String
-}*/
